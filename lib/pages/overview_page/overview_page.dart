441f207071e2f519687cc9fc7112f7f0636c20a3
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-05-29 01:51:34 +0200

    feat: add overview and recipes pages

---
+++ lib/pages/overview_page/overview_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fridge_manager/home/cubit/home_cubit.dart';
+import 'package:fridge_manager/l10n/l10n.dart';
+
+class OverviewPage extends StatelessWidget {
+  const OverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(S.of(context).nothingToSeeHereYet),
+          TextButton(
+            onPressed: () => context.read<HomeCubit>().setTab(HomeTab.products),
+            child: Text(S.of(context).productsPageLink),
+          ),
+        ],
+      ),
+    );
+  }
+}
