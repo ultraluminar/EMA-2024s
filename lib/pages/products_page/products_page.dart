14dca0e0fed0a5d2a0567c082fa4e49ff7bf1001
KlasL4243 <lukasklassen4243@gmail.com>
2024-05-30 01:52:21 +0200

    feat: redirect to overview for now

--- lib/pages/products_page/products_page.dart
+++ lib/pages/products_page/products_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fridge_manager/home/cubit/home_cubit.dart';
+import 'package:fridge_manager/l10n/l10n.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Products Page"),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(S.of(context).nothingToSeeHereYet),
+          TextButton(
+            onPressed: () => context.read<HomeCubit>().setTab(HomeTab.home),
+            child: const Text("Go to Overview"),
+          ),
+        ],
+      ),
     );
   }
 }
