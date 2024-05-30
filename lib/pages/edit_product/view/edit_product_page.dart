148b00c9c419a9c0f8171b3fd65e69ee55a17543
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-05-30 03:20:45 +0200

    feat: add working name field to EditProductPage

--- lib/pages/edit_product/view/edit_product_page.dart
+++ lib/pages/edit_product/view/edit_product_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fridge_manager/l10n/l10n.dart';
 import 'package:fridge_manager/pages/edit_product/edit_product.dart';
 import 'package:products_api/products_api.dart';
 import 'package:products_repository/products_repository.dart';
@@ -12,7 +14,7 @@ class EditProductPage extends StatelessWidget {
       fullscreenDialog: true,
       builder: (context) => BlocProvider(
         create: (context) => EditProductBloc(
-          productRepository: context.read<ProductsRepository>(),
+          productsRepository: context.read<ProductsRepository>(),
           initialProduct: initialProduct,
         ),
         child: const EditProductPage(),
@@ -46,24 +48,81 @@ class EditProductView extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           isNewProduct
-              ? 'Add Product'
-              : 'Edit Product',
+              ? S.of(context).editProductAddAppBarTitle
+              : S.of(context).editProductEditAppBarTitle,
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        tooltip: 'Save Product',
-        shape: const ContinuousRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(32)),
-        ),
+        tooltip: S.of(context).editProductSaveButtonTooltip,
         onPressed: status.isLoadingOrSuccess
             ? null
-            : () => context.read<EditProductBloc>().add(const EditProductSubmitted()),
+            : () => context
+                .read<EditProductBloc>()
+                .add(const EditProductSubmitted()),
         child: status.isLoadingOrSuccess
             ? const CircularProgressIndicator()
             : const Icon(Icons.check_rounded),
       ),
-      body: const Placeholder(),
+      body: const SingleChildScrollView(
+        child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              children: [
+                _NameField(),
+                // _ExpirationDateField(),
+                // _StorageDateField(),
+                // _OwnerField(),
+                // _KategoryField(),
+              ],
+            )),
+      ),
     );
   }
 }
-        
+
+// TODO: Implement some sort of validation for fields so necessary fields are filled out
+class _NameField extends StatelessWidget {
+  const _NameField();
+
+  @override
+  Widget build(BuildContext context) {
+    final state = context.watch<EditProductBloc>().state;
+    final hintText = state.initialProduct?.name ?? '';
+
+    return TextField(
+      key: const Key('editProductForm_nameInput_textField'),
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        enabled: !state.status.isLoadingOrSuccess,
+        labelText: "Test", //S.of(context).editProductFormNameFieldLabel,
+        hintText: hintText,
+      ),
+      maxLength: 50,
+      inputFormatters: [
+        LengthLimitingTextInputFormatter(50),
+        FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z]')),
+      ],
+      onChanged: (name) =>
+          context.read<EditProductBloc>().add(EditProductNameChanged(name)),
+    );
+  }
+}
+
+// class _ExpirationDateField extends StatelessWidget {
+//   const _ExpirationDateField();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final state = context.watch<EditProductBloc>().state;
+    
+//     return TextFormField(
+//       key: const Key('editProductForm_expirationDateInput_textField'),
+//       initialValue: state.expiresAt.toString(),
+//       decoration: InputDecoration(
+//         enabled: !state.status.isLoadingOrSuccess,
+//         labelText: "Test", //S.of(context).editProductFormExpirationDateFieldLabel,
+//       ),
+//       onChanged: (expiresAt) => context
+//           .read<EditProductBloc>()
+//           .add(EditProductExpirationDateChanged(expiresAt)),
+//     );
