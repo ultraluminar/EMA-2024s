dca2ee35ac511eb070c8ad00b724152a09dd781c
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-04 11:04:13 +0200

    feat: add app_ui package

---
+++ packages/app_ui/lib/app_ui.dart
@@ -0,0 +1,3 @@
+library app_ui;
+
+export 'src/widgets/widgets.dart';
---
+++ packages/app_ui/lib/src/widgets/app_date_field.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AppDateField extends StatelessWidget {
+  AppDateField({
+    required this.onDatePicked,
+    required this.dateFormat,
+    required this.labelText,
+    DateTime? initialDate,
+    this.enabled = true,
+    super.key,
+  }) : initialDate = initialDate ?? DateTime.now();
+
+  final ValueChanged<DateTime> onDatePicked;
+  final DateFormat dateFormat;
+  final String labelText;
+  final DateTime initialDate;
+  final bool enabled;
+
+  Future<void> selectDate(BuildContext context) async {
+    showDatePicker(
+      context: context,
+      initialDate: initialDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    ).then(
+      (DateTime? picked) {
+        if (picked != null && picked != initialDate) onDatePicked(picked);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: key,
+      decoration: InputDecoration(
+        border: const OutlineInputBorder(),
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.edit_calendar_rounded),
+          onPressed: () => selectDate(context),
+        ),
+        enabled: enabled,
+        labelText: labelText,
+      ),
+      onTap: () => selectDate(context),
+      controller: TextEditingController(
+        text: dateFormat.format(initialDate),
+      ),
+      readOnly: true,
+    );
+  }
+}
