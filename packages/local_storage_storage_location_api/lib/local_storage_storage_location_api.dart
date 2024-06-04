e56ee321b26b1f6c5b515c28a3f5d950059cd843
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-06-04 18:19:01 +0200

    feat: add local_storage_storage_location_api

---
+++ packages/local_storage_storage_location_api/lib/local_storage_storage_location_api.dart
@@ -0,0 +1,6 @@
+library local_storage_storage_location_api;
+
+export 'package:shared_preferences/shared_preferences.dart'
+    show SharedPreferences;
+
+export 'src/local_storage_storage_location_api.dart';
