bfcf8260e45dc1c119015a28c774671d9f3b7317
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-05 23:30:34 +0200

    style: prettfy DateTimeX file

--- packages/app_utils/lib/src/extentions/datetime.dart
+++ packages/app_utils/lib/src/extentions/datetime.dart
@@ -1,4 +1,9 @@
 extension DateTimeX on DateTime {
-  DateTime get date => this
-      .copyWith(hour: 0, minute: 0, second: 0, millisecond: 0, microsecond: 0);
+  DateTime get date => this.copyWith(
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+        microsecond: 0,
+      );
 }
