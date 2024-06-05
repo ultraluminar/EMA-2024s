1cbf6d43b11f481e559e2c5d3ccebea81573415c
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-05 23:36:31 +0200

    feat: add filter PopumMenu

--- lib/pages/products_page/widgets/products_filter_button.dart
+++ lib/pages/products_page/widgets/products_filter_button.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fridge_manager/pages/products_page/products_page.dart';
 
 class ProductsFilterButton extends StatelessWidget {
   const ProductsFilterButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.filter_list_rounded),
-      onPressed: () => ScaffoldMessenger.of(context)
-        ..hideCurrentSnackBar()
-        ..showSnackBar(
-          const SnackBar(
-            content: Text('Filter button pressed'),
-          ),
+    final currentFilter = context.select<ProductsPageBloc, ProductsViewFilter>(
+        (bloc) => bloc.state.filter);
+
+    return PopupMenuButton<ProductsViewFilter>(
+      shape: const ContinuousRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+      ),
+      initialValue: currentFilter,
+      // tooltip: ,
+      onSelected: (filter) {
+        context
+        .read<ProductsPageBloc>()
+        .add(ProductsPageFilterChanged(filter));
+      },
+      itemBuilder: (context) => const [
+        PopupMenuItem(
+          value: ProductsViewFilter.all,
+          child: Text("all"),
         ),
+        PopupMenuItem(
+          value: ProductsViewFilter.stillGood,
+          child: Text("still good"),
+        ),
+        PopupMenuItem(
+          value: ProductsViewFilter.expired,
+          child: Text("expired"),
+        ),
+      ],
+      icon: const Icon(Icons.filter_1_rounded),
     );
+
+    // return IconButton(
+    //   icon: const Icon(Icons.filter_list_rounded),
+    //   onPressed: () => ScaffoldMessenger.of(context)
+    //     ..hideCurrentSnackBar()
+    //     ..showSnackBar(
+    //       const SnackBar(
+    //         content: Text('Filter button pressed'),
+    //       ),
+    //     ),
+    // );
   }
 }
-
-// TODO: implement PopupMenu
