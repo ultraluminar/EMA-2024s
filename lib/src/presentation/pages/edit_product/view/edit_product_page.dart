ad9fc068b6c091ef373fb823ba6fda38179cf530
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-06-08 01:02:49 +0200

    refactor: renamed app_ui to custom_widgets

--- lib/src/presentation/pages/edit_product/view/edit_product_page.dart
+++ lib/src/presentation/pages/edit_product/view/edit_product_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fridge_manager/l10n/l10n.dart';
-import 'package:fridge_manager/src/presentation/app_ui/app_ui.dart'
+import 'package:fridge_manager/src/presentation/custom_widgets/custom_widgets.dart'
     show AppDateField;
 import 'package:fridge_manager/src/presentation/pages/edit_product/edit_product.dart';
 import 'package:intl/intl.dart';
