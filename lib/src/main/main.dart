a575a965ba91254da183c4f198137c5253dfc9f0
Adrian Lösch <loeschadrianp+git@gmail.com>
2024-06-08 01:06:42 +0200

    refactor: removed bootstrap folder and moved files up into main

--- lib/src/main/main.dart
+++ lib/src/main/main.dart
@@ -1,4 +1,4 @@
-import 'package:fridge_manager/src/main/bootstrap/bootstrap.dart';
+import 'package:fridge_manager/src/main/bootstrap.dart';
 import 'package:fridge_manager/src/presentation/app/app.dart';
 import 'package:local_storage_products_api/local_storage_products_api.dart';
 import 'package:products_repository/products_repository.dart';
