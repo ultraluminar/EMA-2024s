8489bb9369ddb39dd91927de817c4dcc612e9bcc
KlasL4243 <lukasklassen4243@gmail.com>
2024-06-26 23:37:47 +0200

    feat: change overview back to nothing here page

--- lib/src/presentation/pages/overview_page/view/overview_page_view.dart
+++ lib/src/presentation/pages/overview_page/view/overview_page_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:fridge_manager/home/cubit/home_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fridge_manager/l10n/l10n.dart';
-import 'package:fridge_manager/src/presentation/pages/overview_page/local_notification.dart';
+import 'package:fridge_manager/src/presentation/home/cubit/home_cubit.dart';
 
 class OverviewPage extends StatelessWidget {
   const OverviewPage({super.key});
@@ -15,25 +14,46 @@ class OverviewPage extends StatelessWidget {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            ElevatedButton.icon(
-              icon: const Icon(Icons.notifications_none_outlined),
-              onPressed: () {
-                LocalNotification.showSimpleNotification(
-                    title: "Warning!",
-                    body: "Ure mom is about to expire!",
-                    payload: "Take my load!");
-              },
-              label: const Text("Balls Notification"),
-            )
-            // Text(S.of(context).nothingToSeeHereYet),
-            // TextButton(
-            //   onPressed: () =>
-            //       context.read<HomeCubit>().setTab(HomeTab.products),
-            //   child: Text(S.of(context).productsPageLink),
-            // ),
+            Text(S.of(context).nothingToSeeHereYet),
+            TextButton(
+              onPressed: () =>
+                  context.read<HomeCubit>().setTab(HomeTab.products),
+              child: Text(S.of(context).productsPageLink),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text(S.of(context).overviewAppBarTitle)),
+//       body: Center(
+//         child: Column(
+//           mainAxisSize: MainAxisSize.min,
+//           children: [
+//             ElevatedButton.icon(
+//               icon: const Icon(Icons.notifications_none_outlined),
+//               onPressed: () {
+//                 LocalNotification.showSimpleNotification(
+//                     title: "Warning!",
+//                     body: "Ure mom is about to expire!",
+//                     payload: "Take my load!");
+//               },
+//               label: const Text("Balls Notification"),
+//             )
+//             // Text(S.of(context).nothingToSeeHereYet),
+//             // TextButton(
+//             //   onPressed: () =>
+//             //       context.read<HomeCubit>().setTab(HomeTab.products),
+//             //   child: Text(S.of(context).productsPageLink),
+//             // ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
