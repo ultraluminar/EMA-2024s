6cb499eb83087f9eed7bfa020f673354ed591853
KlasL4243 <lukasklassen4243@gmail.com>
2024-07-24 18:18:44 +0200

    feat: implement firebase auth

--- lib/src/main/bootstrap.dart
+++ lib/src/main/bootstrap.dart
@@ -3,8 +3,13 @@ import 'dart:developer';
 
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, GoogleAuthProvider;
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:firebase_ui_oauth_google/firebase_ui_oauth_google.dart';
 import 'package:flutter/material.dart';
+import 'package:fridge_manager/firebase_options.dart';
 import 'package:fridge_manager/src/data/hive_products_api/hive_products_api.dart';
 import 'package:fridge_manager/src/data/hive_settings_api/hive_settings_api.dart';
 import 'package:fridge_manager/src/data/product_name_api/product_name_api.dart';
@@ -16,72 +21,73 @@ import 'package:openfoodfacts/openfoodfacts.dart' hide Product;
 
 typedef AppBuilder = Future<Widget> Function();
 
-const String emulatorIP = "10.0.2.2";
-const int emulatorPort = 8080;
+Future<void> testFirestore(FirebaseFirestore firestore) async {
+  const barcode = "40468259";
+  log("fetch productName");
+  final name = await ProductNameApi.fetch(barcode).timeout(
+    const Duration(seconds: 10),
+    onTimeout: () => "timeouted",
+  );
+
+  final prototype = ProductPrototype.fromScan(name ?? "notFound", barcode)
+      .copyWith(expiresAt: ExpirationDate.today());
+
+  log("define ProductsCollectionRef");
+  final productsCollectionRef = firestore.collection("products");
+  log("save to collection");
+  await productsCollectionRef.add(prototype.toProduct().toJson());
+}
+
+Future<void> setupOpenFoodAPIConfiguration() async {
+  OpenFoodAPIConfiguration.userAgent = UserAgent(
+    name: "Fridge Manager",
+    version: "1.0",
+    system: "Flutter",
+    comment: "An app to manage product expiration dates",
+  );
+  OpenFoodAPIConfiguration.globalLanguages = [OpenFoodFactsLanguage.GERMAN];
+  OpenFoodAPIConfiguration.globalCountry = OpenFoodFactsCountry.GERMANY;
+}
 
 Future<void> bootstrap(AppBuilder builder) async {
   await runZonedGuarded<Future<void>>(
     () async {
-      log("init WidgegBindings");
       WidgetsFlutterBinding.ensureInitialized();
-      log("init DateFormatting");
-      initializeDateFormatting();
-      log("init LocalNotification");
+      await initializeDateFormatting();
+
       await LocalNotification.init();
 
-      log("config OpenFoodApi");
-      OpenFoodAPIConfiguration.userAgent = UserAgent(
-        name: "Fridge Manager",
-        version: "1.0",
-        system: "Flutter",
-        comment: "An app to manage product expiration dates",
-      );
-      OpenFoodAPIConfiguration.globalLanguages = [OpenFoodFactsLanguage.GERMAN];
-      OpenFoodAPIConfiguration.globalCountry = OpenFoodFactsCountry.GERMANY;
+      await setupOpenFoodAPIConfiguration();
 
-      log("init Hive");
       await Hive.initFlutter();
-      log("init HiveSettingsApi");
       await HiveSettingsApi.init();
-      log("init HiveProductsApi");
       await HiveProductsApi.init();
-      log("init ProductNameApi");
       await ProductNameApi.init();
 
-      log("init FirebaseApp");
-      final firebaseApp = await Firebase.initializeApp();
-      log("init Firestore");
-      final FirebaseFirestore firestore =
-          FirebaseFirestore.instanceFor(app: firebaseApp);
-      log("use Firestore Emulator");
-      firestore.useFirestoreEmulator(emulatorIP, emulatorPort);
+      await Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform);
+      FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+      FirebaseUIAuth.configureProviders([
+        EmailAuthProvider(),
+        GoogleProvider(
+            clientId:
+                '688703949293-5tig6q6rtro1hds41d09d865b2pt2so9.apps.googleusercontent.com')
+      ]);
 
-      const barcode = "40468259";
-      log("fetch productName");
-      final name = await ProductNameApi.fetch(barcode).timeout(
-        const Duration(seconds: 10),
-        onTimeout: () => "timeouted",
-      );
+      // await testFirestore(firestore);
 
-      final prototype = ProductPrototype.fromScan(name ?? "notFound", barcode)
-          .copyWith(expiresAt: ExpirationDate.today());
-
-      log("define ProductsCollectionRef");
-      final productsCollectionRef = firestore.collection("products");
-      log("save to collection");
-      await productsCollectionRef.add(prototype.toProduct().toJson());
-      log("init BlocObserver");
       // final analyticsRepository =
       //     AnalyticsRepository(FirebaseAnalytics.instance);
       final blocObserver = AppBlocObserver(
           // analyticsRepository: analyticsRepository,
           );
       Bloc.observer = blocObserver;
-      log("done");
+
       runApp(
         await builder(),
       );
     },
-    (_, __) {},
+    (error, stack) {},
   );
 }
